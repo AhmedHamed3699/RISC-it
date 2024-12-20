78307dea39f259a132a333acd5c4e9ccc969a9ab
AhmedHamed3699 <Ahmed.Hamed03@eng-st.cu.edu.eg>
2024-12-20 08:45:36 +0200

    feat: created instruction memory

---
+++ code/src/rtl/stages/0-fetch/instruction_memory.vhd
@@ -0,0 +1,33 @@
+LIBRARY IEEE;
+USE IEEE.std_logic_1164.ALL;
+USE IEEE.STD_LOGIC_UNSIGNED.ALL;
+USE IEEE.numeric_std.ALL;
+
+ENTITY instruction_memory IS
+    PORT (
+        clk : IN STD_LOGIC;
+        address : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
+        inst : OUT STD_LOGIC_VECTOR(15 DOWNTO 0);
+        saved_addresses : OUT ARRAY(9 DOWNTO 0) OF STD_LOGIC_VECTOR(15 DOWNTO 0);
+    );
+END ENTITY instruction_memory;
+
+ARCHITECTURE instruction_memory_arch OF instruction_memory IS
+    CONSTANT mem_depth : INTEGER := 4096;
+    CONSTANT word_width : INTEGER := 16;
+
+    TYPE ram_type IS ARRAY(mem_depth - 1 DOWNTO 0) OF STD_LOGIC_VECTOR (word_width - 1 DOWNTO 0);
+    SIGNAL ram : ram_type := (OTHERS => (OTHERS => '0'));
+
+BEGIN
+    PROCESS (clk) IS
+        VARIABLE addr_var := ;
+    BEGIN
+        IF RISING_EDGE(clk) THEN
+            IF (TO_INTEGER(UNSIGNED(address)) < mem_depth - 1) THEN
+                inst <= ram(TO_INTEGER(UNSIGNED(address)));
+            END IF;
+        END IF;
+    END PROCESS;
+    saved_addresses <= ram(9 DOWNTO 0);
+END ARCHITECTURE;
