56ba488cb8c0b604b260638a9c36c3cf5eb57392
somiaelshemy <somiaelshemy22@gmail.com>
2024-12-20 20:01:45 +0200

    add pc register

--- code/src/stages/0-fetch/rtl/pc_register.vhd
+++ code/src/stages/0-fetch/rtl/pc_register.vhd
@@ -6,18 +6,26 @@ USE IEEE.numeric_std.ALL;
 ENTITY pc_reg IS
     PORT (
         pc_in : IN STD_LOGIC_VECTOR (15 DOWNTO 0);
-        enable, clk : IN STD_LOGIC;
+        clk, hlt, rst, one_cycle : IN STD_LOGIC;
         next_ins_address : OUT STD_LOGIC_VECTOR (15 DOWNTO 0)
     );
 END ENTITY pc_reg;
 
 ARCHITECTURE pc_reg_arch OF pc_reg IS
+    SIGNAL stop_till_rst : STD_LOGIC := '0';
 BEGIN
     PROCESS (clk)
     BEGIN
         IF (RISING_EDGE(clk)) THEN
-            IF (enable = '0') THEN
-                next_ins_address <= pc_in;
+            IF (rst) THEN
+                stop_till_reset <= '0';
+                next_ins_address <= (OTHERS => '0');
+            ELSIF (NOT stop_till_reset) THEN
+                IF ((NOT one_cycle) AND (NOT hlt)) THEN
+                    next_ins_address <= pc_in;
+                ELSIF hlt THEN
+                    stop_till_reset <= '1';
+                END IF;
             END IF;
         END IF;
     END PROCESS;
