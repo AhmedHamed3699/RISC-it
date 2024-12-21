c327130cefe48d2d97a04a8078204a47023e9475
somiaelshemy <somiaelshemy22@gmail.com>
2024-12-21 09:29:34 +0200

    edit instruction memory

--- code/src/stages/0-fetch/rtl/instruction_memory.vhd
+++ code/src/stages/0-fetch/rtl/instruction_memory.vhd
@@ -4,25 +4,25 @@ USE IEEE.STD_LOGIC_UNSIGNED.ALL;
 USE IEEE.numeric_std.ALL;
 
 ENTITY instruction_memory IS
-    TYPE saved_array_type IS ARRAY (3 DOWNTO 0) OF STD_LOGIC_VECTOR(15 DOWNTO 0);
     PORT (
         clk : IN STD_LOGIC;
         address : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
         inst : OUT STD_LOGIC_VECTOR(15 DOWNTO 0);
-        saved_addresses : OUT saved_array_type
+        empty_stack : OUT STD_LOGIC_VECTOR(15 DOWNTO 0);
+        invalid_mem_add : OUT STD_LOGIC_VECTOR(15 DOWNTO 0);
+        INT0 : OUT STD_LOGIC_VECTOR(15 DOWNTO 0);
+        INT2 : OUT STD_LOGIC_VECTOR(15 DOWNTO 0)
     );
 END ENTITY instruction_memory;
 
 ARCHITECTURE instruction_memory_arch OF instruction_memory IS
     CONSTANT mem_depth : INTEGER := 4096;
     CONSTANT word_width : INTEGER := 16;
-
     TYPE ram_type IS ARRAY(mem_depth - 1 DOWNTO 0) OF STD_LOGIC_VECTOR (word_width - 1 DOWNTO 0);
     SIGNAL ram : ram_type := (OTHERS => (OTHERS => '0'));
 
 BEGIN
     PROCESS (clk) IS
-        VARIABLE addr_var := ;
     BEGIN
         IF RISING_EDGE(clk) THEN
             IF (TO_INTEGER(UNSIGNED(address)) < mem_depth - 1) THEN
@@ -30,5 +30,8 @@ BEGIN
             END IF;
         END IF;
     END PROCESS;
-    saved_addresses <= ram(3 DOWNTO 0);
+    empty_stack <= ram(0);
+    invalid_mem_add <= ram(1);
+    INT0 <= ram(2);
+    INT2 <= ram(3);
 END ARCHITECTURE;
