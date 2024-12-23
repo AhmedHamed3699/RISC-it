f6c5074b65e3a25a784f887ad58c687d382278e5
somiaelshemy <somiaelshemy22@gmail.com>
2024-12-23 14:38:30 +0200

    fix pc_plus signal and hlt logic

--- code/src/stages/0-fetch/rtl/fetch_stage.vhd
+++ code/src/stages/0-fetch/rtl/fetch_stage.vhd
@@ -45,7 +45,6 @@ ARCHITECTURE fetch_stage_arch OF fetch_stage IS
   SIGNAL one_cycle : STD_LOGIC := '0';
   SIGNAL read_address_in : STD_LOGIC_VECTOR(15 DOWNTO 0) := (OTHERS => '0');
   SIGNAL instruction_sig : STD_LOGIC_VECTOR (15 DOWNTO 0) := (OTHERS => '0');
-  SIGNAL stop_till_rst : STD_LOGIC := '0';
 
   SIGNAL one : STD_LOGIC_VECTOR (15 DOWNTO 0) := (0 => '1', OTHERS => '0');
   SIGNAL pc_plus : STD_LOGIC_VECTOR (15 DOWNTO 0) := (OTHERS => '0');
@@ -83,6 +82,7 @@ BEGIN
 
   ---------PORT MAPPING---------
   pc_plus <= STD_LOGIC_VECTOR(unsigned(read_address_in) + unsigned(one));
+  one_cycle <= STALL OR RTI OR INT;
   ins_mem_inst : instruction_memory PORT MAP(clk, read_address_in, instruction_sig, IM0, IM2, IM4, IM6, IM8);
   branching_mux : mux2to1_16bit PORT MAP(pc_plus, JMP_inst, BRANCH, branch_mux_out);
   index_mux : mux2to1_16bit PORT MAP(IM6, IM8, INDEX, ind_mux_out);
@@ -91,29 +91,25 @@ BEGIN
   ex_mux : mux2to1_16bit PORT MAP(ex_mem_int_mux_out, exp_mux_out, EX, ex_mux_out);
   rst_mux : mux2to1_16bit PORT MAP(ex_mux_out, IM0, RST, rst_mux_out);
 
-  PROCESS (RST)
+  PROCESS (clk, RST)
+    VARIABLE stop_till_rst : BIT := '0';
   BEGIN
-    IF (RST = '1') THEN
-      read_address_in <= IM0;
-    END IF;
-  END PROCESS;
 
-  PROCESS (clk)
-  BEGIN
+    IF (RST = '1') THEN
+      stop_till_rst := '0';
+      read_address_in <= IM0;
+      pc <= IM0;
+    END IF;
     IF (RISING_EDGE(clk)) THEN
-      one_cycle <= STALL OR RTI OR INT;
-      IF (RST = '1') THEN
-        stop_till_rst <= '0';
-        pc <= IM0;
-        read_address_in <= IM0;
-        pc_plus <= STD_LOGIC_VECTOR(unsigned(IM0) + unsigned(one));
-      ELSIF (stop_till_rst = '0') THEN
-        IF (one_cycle = '0' AND hlt = '0') THEN
-          pc <= rst_mux_out;
-        ELSIF hlt = '1' THEN
-          stop_till_rst <= '1';
-        END IF;
+      IF (HLT = '1') THEN
+        stop_till_rst := '1';
+      END IF;
+      IF (stop_till_rst = '0' AND one_cycle = '0') THEN
+        pc <= rst_mux_out;
+        read_address_in <= rst_mux_out;
+        instruction <= instruction_sig;
       END IF;
     END IF;
   END PROCESS;
+
 END fetch_stage_arch;
