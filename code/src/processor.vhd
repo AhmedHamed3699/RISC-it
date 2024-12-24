502b653e729c79b70b445cec13ad9430d1fe348d
somiaelshemy <somiaelshemy22@gmail.com>
2024-12-24 23:08:42 +0200

    add stage register signals

--- code/src/processor.vhd
+++ code/src/processor.vhd
@@ -71,6 +71,7 @@ ARCHITECTURE risc_processor_arch OF risc_processor IS
 
   COMPONENT execute_stage IS
     PORT (
+
       -- Inputs
       clk : IN STD_LOGIC;
       src1_addr, src2_addr : IN STD_LOGIC_VECTOR (2 DOWNTO 0);
@@ -81,6 +82,9 @@ ARCHITECTURE risc_processor_arch OF risc_processor IS
       Imm : IN STD_LOGIC_VECTOR (15 DOWNTO 0);
       flags_in : IN STD_LOGIC_VECTOR (3 DOWNTO 0);
       in_port : IN STD_LOGIC_VECTOR (15 DOWNTO 0);
+      pc_in : IN STD_LOGIC_VECTOR (15 DOWNTO 0);
+      Rdst_addr_in : IN STD_LOGIC_VECTOR (2 DOWNTO 0);
+
       -- Control signals
       flag_restore : IN STD_LOGIC;
       alu_operation : IN STD_LOGIC_VECTOR (2 DOWNTO 0);
@@ -92,12 +96,16 @@ ARCHITECTURE risc_processor_arch OF risc_processor IS
       branch : IN STD_LOGIC;
       mem_read : IN STD_LOGIC;
       mem_write : IN STD_LOGIC;
+
       -- Outputs
+      pc_out : OUT STD_LOGIC_VECTOR (15 DOWNTO 0);
+      Rdst_addr_out : IN STD_LOGIC_VECTOR (2 DOWNTO 0);
+
       out_port : OUT STD_LOGIC_VECTOR (15 DOWNTO 0);
       will_jmp : OUT STD_LOGIC;
       mem_excep : OUT STD_LOGIC;
       flags_out : OUT STD_LOGIC_VECTOR (3 DOWNTO 0);
-      res : OUT STD_LOGIC_VECTOR (15 DOWNTO 0)
+      res : OUT STD_LOGIC_VECTOR (15 DOWNTO 0);
     );
   END COMPONENT;
 
@@ -123,7 +131,33 @@ ARCHITECTURE risc_processor_arch OF risc_processor IS
   SIGNAL JMP_inst, instruction, pc : STD_LOGIC_VECTOR (15 DOWNTO 0);
   SIGNAL flush : STD_LOGIC;
 
-  -- Stages Registers
+  ---------STAGE REGISTERS---------
+  SIGNAL FD_in, FD_out : STD_LOGIC_VECTOR (31 DOWNTO 0);
+  SIGNAL DE_in, DE_out : STD_LOGIC_VECTOR (127 DOWNTO 0);
+  SIGNAL EM_in, EM_out : STD_LOGIC_VECTOR (63 DOWNTO 0);
+  SIGNAL MW_in, MW_out : STD_LOGIC_VECTOR (63 DOWNTO 0);
+
+  SIGNAL FD_inst : STD_LOGIC_VECTOR (15 DOWNTO 0);
+  SIGNAL FD_pc_in : STD_LOGIC_VECTOR (15 DOWNTO 0);
+
+  SIGNAL DE_src1_addr : STD_LOGIC_VECTOR (2 DOWNTO 0);
+  SIGNAL DE_Rdst_addr_out : STD_LOGIC_VECTOR (2 DOWNTO 0);
+  SIGNAL DE_Rsrc1 : STD_LOGIC_VECTOR (15 DOWNTO 0);
+  SIGNAL DE_sig : STD_LOGIC_VECTOR (20 DOWNTO 0);
+  SIGNAL DE_pc_in : STD_LOGIC_VECTOR (15 DOWNTO 0);
+  SIGNAL DE_src2_addr : STD_LOGIC_VECTOR (2 DOWNTO 0);
+  SIGNAL DE_Rsrc1 : STD_LOGIC_VECTOR (15 DOWNTO 0);
+
+  SIGNAL EM_sig : STD_LOGIC_VECTOR (6 DOWNTO 0);
+  SIGNAL EM_Rsrc1 : STD_LOGIC_VECTOR (15 DOWNTO 0);
+  SIGNAL EM_Res : STD_LOGIC_VECTOR (15 DOWNTO 0);
+  SIGNAL EM_Rdest_addr : STD_LOGIC_VECTOR (2 DOWNTO 0);
+  SIGNAL EM_pc : STD_LOGIC_VECTOR (15 DOWNTO 0);
+
+  SIGNAL MW_sig : STD_LOGIC_VECTOR (1 DOWNTO 0);
+  SIGNAL MW_mem : STD_LOGIC_VECTOR (15 DOWNTO 0);
+  SIGNAL MW_res : STD_LOGIC_VECTOR (15 DOWNTO 0);
+  SIGNAL MW_Rdest_addr : STD_LOGIC_VECTOR (2 DOWNTO 0);
 
 BEGIN
 
@@ -221,7 +255,7 @@ BEGIN
     final_result => final_result
   );
 
-  PROCESS (clk, rst)
+  PROCESS (clk)
   BEGIN
     -- sycnronous code
   END PROCESS;
